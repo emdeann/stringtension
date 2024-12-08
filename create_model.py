426c94c126ab204360bc3266ac295b54bfd69285
Dean <me@deann.dev>
2024-12-08 11:14:35 -0800

    Retrain model

--- create_model.py
+++ create_model.py
@@ -22,15 +22,29 @@ def load_images_and_labels(data_dir):
                 img = transform.resize(img, (64, 64))
                 images.append(img.flatten())
                 labels.append(label)
-    print(np.array(images))
     return np.array(images), np.array(labels)
 
 
 # Load data from the directory
-data_dir = 'Practice/clf-data'
+data_dir = 'standing_wave_images'
 images, labels = load_images_and_labels(data_dir)
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
     images, labels, test_size=0.2, random_state=42
 )
+
+# Test different values of 'gamma' and 'C'
+classifier = SVC()
+grid = GridSearchCV(classifier, [{'gamma': [0.01, 0.001, 0.0001], 'C': [1, 10, 100, 1000]}])
+grid.fit(X_train, y_train)
+best_model = grid.best_estimator_
+
+y_prediction = best_model.predict(X_test)
+best_accuracy = accuracy_score(y_prediction, y_test)
+
+# Print the best accuracy score
+print(f"Best Accuracy: {best_accuracy}")
+
+# Save the model
+joblib.dump(best_model, 'standing_wave_classifier.pkl', compress=3)
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import get_frequency
+import joblib
+from sklearn import svm
+from skimage import io, color, transform
+
+#model = joblib.load('model.p') # load pretrained model for n-prediction
+img = io.imread('string.png')
+img = color.rgb2gray(img[...,0:3])
+img = transform.resize(img, (64, 64))
+img = img.flatten()
