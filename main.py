8e9dcbf90709bfe6636eb696a8a16e6dcf5722e9
Dean <me@deann.dev>
2024-12-08 12:13:07 -0800

    Add error handling for incorrect argument length

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import numpy as np
+import sys
 from get_frequency import find_frequency
 import joblib
 from sklearn import svm
 from skimage import io, color, transform
 
-model = joblib.load('standing_wave_classifier.pkl') # load pretrained model for n-prediction
 
-image_path = 'string2.png'
-img = io.imread(image_path)
-img = color.rgb2gray(img[...,0:3])
-img = transform.resize(img, (64, 64))
-img = img.flatten()
-arr = np.array([img]).reshape(1,-1)
-n = model.predict(arr)[0] + 1# get first (only) element from model prediction based on image
-print(f'Wave harmonic: {n}')
+def calculate_tension():
+    model = joblib.load('standing_wave_classifier.pkl') # load pretrained model for n-prediction
+    
+    image_path = sys.argv[1]
+    img = io.imread(image_path)
+    img = color.rgb2gray(img[...,0:3])
+    img = transform.resize(img, (64, 64))
+    img = img.flatten()
+    arr = np.array([img]).reshape(1,-1)
+    n = model.predict(arr)[0] + 1# get first (only) element from model prediction based on image
+    print(f'Wave harmonic: {n}')
 
-video_path = 'Videos/f2.mp4'
-video_framerate = 304
-f = find_frequency(video_path, n, video_framerate)
-print(f'Calculated Frequency: {f} Hz')    
+    video_path = sys.argv[2]
+    video_framerate = 304
+    f = find_frequency(video_path, n, video_framerate)
+    print(f'Calculated Frequency: {f} Hz')    
 
-# --- Final Calculation
-L = 2.0 # m
-m = 6.11e-4 # kg/m
+    # --- Final Calculation
+    L = 2.0 # m
+    m = 6.11e-4 # kg/m
 
-T = 4 * ((L * (f/n)) ** 2) * m
-print(f'Tension in the string: {T} N')
+    T = 4 * ((L * (f/n)) ** 2) * m
+    print(f'Tension in the string: {T} N')
+
+
+if len(sys.argv) == 3:
+    calculate_tension()
+else:
+    print('Incorrect arguments passed. Calls should look like `st imgname.ext videoname.ext`')
+    
